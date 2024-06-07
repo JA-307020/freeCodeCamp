a2ec555d7941e99645898e6f01fcd3da8baea2a6
Flynn <48786338+JA-307020@users.noreply.github.com>
2024-06-07 12:01:51 +0300

    saving work

---
+++ JavaScript/telephone-number-validator/script.js
@@ -0,0 +1,7 @@
+const userInput = document.getElementById('user-input');
+const results = document.getElementById('results-div');
+const checkBtn = document.getElementById('check-btn');
+const clearResults = document.getElementById('clear-btn');
+
+
+//TODO: Add input check and event listeners
