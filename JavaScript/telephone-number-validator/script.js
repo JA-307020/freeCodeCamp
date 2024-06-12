b7a7812a1ca5acd614401ee7494b610bf3028117
Flynn <48786338+JA-307020@users.noreply.github.com>
2024-06-12 12:39:57 +0300

    Final version + manual tests

--- JavaScript/telephone-number-validator/script.js
+++ JavaScript/telephone-number-validator/script.js
@@ -3,5 +3,29 @@ const results = document.getElementById('results-div');
 const checkBtn = document.getElementById('check-btn');
 const clearResults = document.getElementById('clear-btn');
 
+const validateInput = () => {
+  const regex = /^(1?[\s]?)(\(\d{3}\)|(\d{3}))([\s-]?\d{3})([\s-]\d{4})$|^(\d{10})$/gi;
+  if (userInput.value === "") {
+    alert("Please provide a phone number");
+  } else if (regex.test(userInput.value)) {
+    results.innerHTML += `<p class="valid">Valid US number:<br>${userInput.value}</p>`;
+  } else {
+    results.innerHTML += `<p class="invalid">Invalid US number:<br>${userInput.value}</p>`;
+  }
+}
 
-//TODO: Add input check and event listeners
+const resetInput = () => userInput.value = "";
+
+checkBtn.addEventListener("click", () => {  
+  validateInput();
+  resetInput();
+})
+
+userInput.addEventListener("keydown", (e) => {
+  if(e.key === "Enter") {
+    validateInput();
+    resetInput()
+  }
+})
+
+clearResults.addEventListener("click", () => results.innerHTML = "");
