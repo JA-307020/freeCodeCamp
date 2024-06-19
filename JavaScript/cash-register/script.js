1483ba77e555b103b700f27c8174b5b44bd726be
Flynn <48786338+JA-307020@users.noreply.github.com>
2024-06-19 12:03:02 +0300

    saving work

--- JavaScript/cash-register/script.js
+++ JavaScript/cash-register/script.js
@@ -1,8 +1,8 @@
 const changeDue = document.getElementById("change-due");
 const cash = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
+const availableCurrency = document.getElementById("available-currency");
 let price = 1.87;
-document.getElementsByClassName("display").innerHTML = `<p>Total: ${price}</p>`;
 let cid = [
   ["PENNY", 1.01],
   ["NICKEL", 2.05],
@@ -27,20 +27,39 @@ const currency = [
   ["ONE HUNDRED", 100]
 ];
 
+const availableCid = cid.slice();
+
+
+// const previewCid = () => {
+//   availableCurrency.innerHTML = `
+//     <li>Pennies: ${availableCid[0][1]}</li>
+//     <li>Nickels: ${availableCid[1][1]}</li>
+//     <li>Dimes: ${availableCid[2][1]}</li>
+//     <li>Quarters: ${availableCid[3][1]}</li>
+//     <li>Ones: ${availableCid[4][1]}</li>
+//     <li>Fives: ${availableCid[5][1]}</li>
+//     <li>Tens: ${availableCid[6][1]}</li>
+//     <li>Twenties: ${availableCid[7][1]}</li>
+//     <li>Hundreds: ${availableCid[8][1]}</li>
+//     `
+// }
+
+
 const initialCheck = () => {
-  if (cash.value < price) {
+  const cashFromCustomer = Number.parseFloat(cash.value);
+  if (cashFromCustomer < price) {
     alert("Customer does not have enough money to purchase this item");
     return;
   }
-  if (cash.value === price) {
+  if (cashFromCustomer === price) {
     changeDue.innerHTML = `<p>No change due - customer paid with exact cash</p>`;
     return;
   }
+  const changeToGive = cashFromCustomer - price;
+  return changeToGive;
 }
 
 purchaseBtn.addEventListener("click", () => {
   initialCheck();
-
+  console.log(initialCheck());
 })
-
-//TODO: fix the price display
